a28dfb7000e3cc5c36190b010d27cac2f912d008
A01748505 <89160214+A01748505@users.noreply.github.com>
2021-08-19 15:29:53 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,5 +1,8 @@
 def main():
     #escribe tu código abajo de esta línea
+    m=float(input("Dame los minutos: " ))
+    m_x_cm= m*5.7*6
+    print("Centímentros recorridos: "+str(m_x_cm))
     pass
 
 if __name__ == '__main__':
