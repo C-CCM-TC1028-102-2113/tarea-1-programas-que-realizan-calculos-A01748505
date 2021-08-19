35038f66565adebd1e9b740589648933745230e3
A01748505 <89160214+A01748505@users.noreply.github.com>
2021-08-19 15:39:15 -0500

    Update exercise.py

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,6 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
     #Leer los datos
+    nm=int(input("Dame el número de mensajes: "))
+    nme=float(input("Dame el número de megas: "))
+    nmin=int(input("Dame el número de minutos: "))
+    numm=nm*0.8
+    numme=nme*0.8
+    numin=nmin*0.8
+    cm=numm+numme+numin
+print("El costo mensual es: ",cm)
     pass
 
 
