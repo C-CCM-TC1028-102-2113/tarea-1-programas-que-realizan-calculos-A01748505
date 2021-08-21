d58780f5865fd39a661301ae4b767008fb9cbe3c
A01748505 <89160214+A01748505@users.noreply.github.com>
2021-08-21 11:03:16 -0500

    Update exercise.py

--- assignments/21LevaduraParaPizza/src/exercise.py
+++ assignments/21LevaduraParaPizza/src/exercise.py
@@ -1,5 +1,8 @@
 def main():
     #escribe tu código abajo de esta línea
+    gramos=float(input("Dame la harina en gramos: "))
+    levadura=(((gramos/1000))*50)
+    print("Ncesitas estos gramos de levadura: ",levadura)
     pass
 
 
